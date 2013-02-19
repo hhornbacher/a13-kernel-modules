093fa8741af6c2c4ec8d40739df7c35859bf4530
hhornbacher <h.hornbacher@gmail.com>
2013-02-19 16:45:07 +0100

    Test code for GPIO experimenting

--- gpio-test/gpio-test.c
+++ gpio-test/gpio-test.c
@@ -12,11 +12,14 @@
 
 #include "gpio-test.h"
 
-static unsigned gpio_handler;
-static script_gpio_set_t info;
+static unsigned gpio_handler1,gpio_handler2;
+static script_gpio_set_t info1,info2;
+
+static int 
 
 static void __exit gpio_test_exit_driver(void) {
-    gpio_release(gpio_handler, 0);
+    gpio_release(gpio_handler1, 0);
+    gpio_release(gpio_handler2, 0);
     printk(KERN_INFO "%s: Unloaded GPIO test driver\n", __func__);
 }
 
@@ -24,17 +27,39 @@ static int __init gpio_test_init_driver(void) {
     int err = 0;
 
     // Get GPIO Pin config from script.bin
-    err = script_parser_fetch("gpt_para", "gpt_pin", (int *) &info, sizeof (script_gpio_set_t));
-    gpio_handler = gpio_request_ex("gpt_para", "gpt_pin");
-    if (!gpio_handler || err) {
-        printk(KERN_INFO "%s can not get \"gpt_para\" \"gpt_pin\" gpio handler, already used by others?", __FUNCTION__);
+    err = script_parser_fetch("gpt_para", "gpt_pin_1", (int *) &info1, sizeof (script_gpio_set_t));
+    gpio_handler1 = gpio_request_ex("gpt_para", "gpt_pin_1");
+    if (!gpio_handler1 || err) {
+        printk(KERN_INFO "%s: can not get \"gpt_para\" \"gpt_pin_1\" gpio handler, already used by others?", __FUNCTION__);
+        goto exit;
+    }
+    
+    // Get GPIO Pin config from script.bin
+    err = script_parser_fetch("gpt_para", "gpt_pin_2", (int *) &info2, sizeof (script_gpio_set_t));
+    gpio_handler2 = gpio_request_ex("gpt_para", "gpt_pin_2");
+    if (!gpio_handler2 || err) {
+        printk(KERN_INFO "%s: can not get \"gpt_para\" \"gpt_pin_2\" gpio handler, already used by others?", __FUNCTION__);
+        goto exit;
+    }
+    
+    
+
+    // Set GPIO to output
+    err = gpio_set_one_pin_io_status(gpio_handler1, 1, NULL);
+    if (err!=0) {
+        printk(KERN_INFO "%s: Failed to set GPIO io status to output.", __FUNCTION__);
         goto exit;
     }
 
-    // Set GPIO to output
-    gpio_set_one_pin_io_status(gpio_handler, 1, info.gpio_name);
-
-    gpio_write_one_pin_value(gpio_handler, 1, info.gpio_name);
+    err = gpio_write_one_pin_value(gpio_handler1, 1, NULL);
+    if (err!=0) {
+        printk(KERN_INFO "%s: Failed to write to GPIO pin.", __FUNCTION__);
+        goto exit;
+    }
+    int data = gpio_read_one_pin_value(gpio_handler1, NULL);
+    printk(KERN_INFO "%s: GPIO pin1 value: %d", __FUNCTION__, data );
+    int data = gpio_read_one_pin_value(gpio_handler2, NULL);
+    printk(KERN_INFO "%s: GPIO pin2 value: %d", __FUNCTION__, data );
 
     printk(KERN_INFO "%s: Loaded GPIO test driver\n", __func__);
     return 0;
